1e5485682b736ed27cedbff3fbfb6778c7a0a271
Enrique Paz Perez <enrique.pazperez@spilgames.com>
2014-01-27 18:53:47 +0100

    OPT lots of progress and simplifications

--- src/erl_cache_server.erl
+++ src/erl_cache_server.erl
@@ -12,8 +12,8 @@
 
 -export([
     start_link/0,
-    get/1,
-    set/4,
+    get/2,
+    set/6,
     evict/1,
     get_stats/0
 ]).
@@ -31,12 +31,15 @@
     stats::dict()            %% Statistics about cache hits
 }).
 
--record(entry, {
+-record(cache_entry, {
     key::term(),
     value::term(),
     created::pos_integer(),
-    ttl::pos_integer(),
-    evict::pos_integer()
+    validity::pos_integer(),
+    evict::pos_integer(),
+    validity_delta::pos_integer(),
+    evict_delta::pos_integer(),
+    refresh_callback::erl_cache:refresh_function()
 }).
 
 -define(ETS_CACHE_TABLE, erl_cache_server_table).
@@ -51,59 +54,64 @@
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
--spec get(erl_cache_facade:key()) ->
-    {hit, erl_cache_facade:value()} |
-    {stale, erl_cache_facade:value()} |
-    {evict, erl_cache_facade:value()} |
-    {miss}.
-get(Key) ->
+-spec get(erl_cache:key(), boolean()) ->
+    {ok, {hit, erl_cache:value()} | {stale, erl_cache:value()} | evicted | miss}.
+get(Key, WaitForRefresh) ->
     Now = elibs_time:now_ms(os:timestamp()),
-    case ets:lookup(?ETS_CACHE_TABLE, Key) of
-        [#entry{ttl=Ttl, value=Value}] when Now < Ttl ->
-            update_cache_stats(Key, hit),
+    Res = case ets:lookup(?ETS_CACHE_TABLE, Key) of
+        [#cache_entry{validity=Validity, value=Value}] when Now < Validity ->
+            gen_server:cast(?MODULE, {set_stat, Key, hit}),
             {hit, Value};
-        [#entry{evict=Evict, value=Value}] when Now < Evict ->
-            update_cache_stats(Key, stale),
+        [#cache_entry{evict=Evict, value=Value, refresh_callback=undefined}] when Now < Evict ->
+            gen_server:cast(?MODULE, {set_stat, Key, stale}),
             {stale, Value};
-        [#entry{value=Value}] ->
-            update_cache_stats(Key, evict),
-            {evict, Value};
+        [#cache_entry{evict=Evict, refresh_callback=Cb}=Entry] when Now < Evict, Cb /=undefined ->
+            ?DEBUG("Refreshing stale key ~p", [Key]),
+            gen_server:cast(?MODULE, {set_stat, Key, stale}),
+            {ok, NewVal} = refresh(Entry, WaitForRefresh),
+            {stale, NewVal};
+        [#cache_entry{value=_ExpiredValue}] ->
+            evict(Key),
+            evicted;
         [] ->
-            update_cache_stats(Key, miss),
-            {miss}
+            gen_server:cast(?MODULE, {set_stat, Key, miss}),
+            miss
+    end,
+    {ok, Res}.
+
+-spec set(erl_cache:key(), erl_cache:value(), pos_integer(), non_neg_integer(),
+          erl_cache:refresh_function(), boolean()) -> ok.
+set(Key, Value, ValidityDelta, EvictDelta, RefreshCb, WaitTillSet) ->
+    Now = elibs_time:now_ms(os:timestamp()),
+    Entry = #cache_entry{
+        key = Key,
+        value = Value,
+        created = Now,
+        validity = Now + ValidityDelta,
+        evict = Now + ValidityDelta + EvictDelta,
+        validity_delta = ValidityDelta,
+        evict_delta = EvictDelta,
+        refresh_callback = RefreshCb
+    },
+    case WaitTillSet of
+        false -> ok = gen_server:cast(?SERVER, {set, Entry});
+        true -> ok = gen_server:call(?SERVER, {set, Entry})
     end.
 
--spec set(erl_cache_facade:key(), erl_cache_facade:value(), pos_integer(), non_neg_integer()) -> ok.
-set(Key, Value, TtlDelta, EvictDelta) ->
-    Now = elibs_time:now_ms(os:timestamp()),
-    Ttl = Now + TtlDelta,
-    Evict = Ttl + EvictDelta,
-    Entry = #entry{
-        key=Key,
-        value=Value,
-        created=Now,
-        ttl=Ttl,
-        evict=Evict
-    },
-    gen_server:cast(?SERVER, {set, Entry}).
-
--spec evict(erl_cache_facade:key()) -> ok.
+-spec evict(erl_cache:key()) -> ok.
 evict(Key) ->
     gen_server:cast(?SERVER, {evict, Key}).
 
-
 %% @doc Retrieve stats for cache server usage. Great for testing and
-%% to ensure nothing is broken. Obviously doesn't work if the application
-%% was not yet started (since the ETS would not have been created).
+%% to ensure nothing is broken.
 -spec get_stats() -> proplists:proplist().
 %% @end
 get_stats() ->
     Info = ets:info(?ETS_CACHE_TABLE),
     Memory = proplists:get_value(memory, Info, 0),
     Entries = proplists:get_value(size, Info, 0),
-    %Keys = lists:flatten(lists:usort(ets:match(?ETS_CACHE_TABLE,{'$1','_'}))),    %ensure asc order
-    %Stats = gen_server:call(?MODULE, get_stats, 500),
-    [{entries, Entries}, {memory, Memory}].
+    ServerStats = gen_server:call(?SERVER, get_stats),
+    [{entries, Entries}, {memory, Memory}] ++ ServerStats.
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
@@ -123,25 +131,26 @@ init([]) ->
 
 -spec handle_call(term(), term(), #state{}) ->
     {reply, Data::any(), #state{}}.
+handle_call({set, #cache_entry{key=Key}=Entry}, _From, #state{stats=StatsDict} = State) ->
+    UpdatedStats = set_cache_entry(Key, Entry, StatsDict),
+    {reply, ok, State#state{stats=UpdatedStats}};
 handle_call(get_stats, _From, #state{stats=Stats} = State) ->
     {reply, dict:to_list(Stats), State};
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
 -spec handle_cast(any(), #state{}) -> {noreply, #state{}}.
-handle_cast({set_stats, Key, Stats}, #state{stats=StatsDict} = State) ->
+handle_cast({set_stat, Key, Stat}, #state{stats=Stats} = State) ->
     {noreply, State#state{
-        stats=update_stats(Key, StatsDict, Stats)
+        stats=update_stats(Key, Stat, Stats)
     }};
-handle_cast({set, #entry{key=Key}=Entry}, #state{stats=StatsDict} = State) ->
-    ets:insert(?ETS_CACHE_TABLE, Entry),
-    {noreply, State#state{
-        stats=update_stats(Key, StatsDict, [{cache, set}])
-    }};
+handle_cast({set, #cache_entry{key=Key}=Entry}, #state{stats=StatsDict} = State) ->
+    UpdatedStats = set_cache_entry(Key, Entry, StatsDict),
+    {noreply, State#state{stats=UpdatedStats}};
 handle_cast({evict, Key}, #state{stats=StatsDict} = State) ->
     ets:delete(?ETS_CACHE_TABLE, Key),
     {noreply, State#state{
-        stats=update_stats(Key, StatsDict, [{cache, evict}])
+        stats=update_stats(Key, evict, StatsDict)
     }};
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -162,21 +171,33 @@ code_change(_OldVsn, State, _Extra) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-%% @doc Update the stats dictionary. Per site keeps track of cached/expired stats.
--spec update_stats(term(), dict(), proplists:proplist()) -> dict().
-%% @end
-update_stats(_, Dict, []) ->
-    Dict;
-update_stats(Key, Dict, [Stat|RestStats]) ->
-    NewDict = dict:update_counter({Key, Stat}, 1, Dict),
-    update_stats(Key, NewDict, RestStats).
+-spec set_cache_entry(erl_cache:key(), dict(), #cache_entry{}) -> dict().
+set_cache_entry(Key, Entry, Stats) ->
+    ets:insert(?ETS_CACHE_TABLE, Entry),
+    update_stats(Key, set, Stats).
 
+-spec refresh(#cache_entry{}, boolean()) ->
+    {ok, erl_cache:value()}.
+refresh(#cache_entry{key=Key, validity_delta=ValidityDelta, evict_delta=EvictDelta,
+               refresh_callback=Callback}, true) when Callback/=undefined ->
+    NewVal = do_apply(Callback),
+    ok = set(Key, NewVal, ValidityDelta, EvictDelta, Callback, true),
+    {ok, NewVal};
+refresh(#cache_entry{key=Key, value=Value, validity_delta=ValidityDelta, evict_delta=EvictDelta,
+               refresh_callback=Callback}, false) when Callback/=undefined ->
+    F = fun () ->
+            NewVal = do_apply(Callback),
+            set(Key, NewVal, ValidityDelta, EvictDelta, Callback, false)
+    end,
+    _ = spawn(F),
+    {ok, Value}.
 
-%% @doc Update the stats dictionary. Per site keeps track of hit/miss stats.
-%% IMPORTANT: only enabled for TEST purposes.
--spec update_cache_stats(term(), hit|miss|stale|set|evict) -> ok.
-%% @end
-update_cache_stats(Key, Result) ->
-    ?DEBUG("erl_cache ~p for key=~p", [Result, Key]),
-    gen_server:cast(?SERVER, {set_stats, Key, [{cache, Result}]}),
-    ok.
+-spec do_apply(mfa() | function()) -> term().
+do_apply({M, F, A}) when is_atom(M), is_atom(F), is_list(A) ->
+    apply(M, F, A);
+do_apply(F) when is_function(F) ->
+    F().
+
+-spec update_stats(erl_cache:key(), hit|miss|stale|evict, dict()) -> dict().
+update_stats(Key, Stat, Stats) ->
+    dict:update_counter({Key, Stat}, 1, Stats).
